2f713194ee73f480b7fb344a5030296c91d6bd0d
TusharBhoir60 <tushar.2403214103@vcet.edu.in>
2025-07-28 21:58:17 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class hash{
+    public static int hasing(String key){
+        int hashtable = 10;
+        int hash = 0;
+        for (int i = 0; i < key.length(); i++) {
+            char ch = key.charAt(i); //takes char value at each index
+            hash += (int)ch;  //Converts char value to ascii value and adds it to hash variable
+        }
+        return hash%hashtable;  //reminder after dividing by hastable value 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter characters:");
+        String inp = sc.nextLine();
+        int hash =  hasing(inp);
+            System.out.println("After hashing " + hash);
+        sc.close();
+    }
+}
+
